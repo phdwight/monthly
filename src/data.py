411415112ccc3375b12736231e12cedb625517a5
phdwight <phdwight@live.com>
2024-05-18 17:08:29 +0800

    Compute monthly bills version 2 (#2)
    * update computation
    
    update
    
    * Corrected the adjusted totals
    
    - corrected the adjusted percentage and amount
    - output to a file
    
    * Updated code to adjust the naming in the yaml file
    
    * addition of water bill
    
    * added value for April 2024
    
    * add capability to output to csv
    
    * added a column to csv for electric only totals
    
    * added the computation for internet
    
    * fix constants
    
    * updated readings
    
    removed csv
    
    * added the veco value for april
    
    added the veco value for april
    
    * version 2.0
    
    * update table computation
    
    * update
    
    manual coding with help from copilot
    
    * OOP version
    
    * version 2 complete

---
+++ src/data.py
@@ -0,0 +1,4 @@
+
+
+data = [{"month":"May", "electric": 5000, "water": 2000, "readings": {"Papa": 1000, "Mama": 2000, "Anak": 3000}},
+        {"month":"June", "electric": 5000, "water": 2000, "readings": {"Papa": 1000, "Mama": 2000, "Anak": 3000}},]
